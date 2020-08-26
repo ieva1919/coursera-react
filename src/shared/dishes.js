e4f3bac48bb1ec1df34ae9e06d40de4a10bfb87d
Ieva Sinkeviciute <ieva1919@gmail.com>
2020-08-26 23:17:37 +0300

    add new info in index.js file

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -7,7 +7,44 @@ export const DISHES =
             category: 'mains',
             label: 'Hot',
             price: '4.99',
-            description: 'A burger with a beef patty, usually depicted on a sesame bun with cheese, lettuce, and tomato—making it a cheeseburger, though officially called Hamburger by Unicode'
+            description: 'A burger with a beef patty, usually depicted on a sesame bun with cheese, lettuce, and tomato—making it a cheeseburger, though officially called Hamburger by Unicode',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+            ]
         },
         {
             id: 1,
@@ -16,7 +53,44 @@ export const DISHES =
             category: 'mains',
             label: 'Hot',
             price: '6.99',
-            description: 'The pizza derives from the concept that pizza rolls must be eaten in one bite, similar to the way a shot of alcohol should be consumed. A pizza shot must be taken all at once to avoid the wonderful'
+            description: 'The pizza derives from the concept that pizza rolls must be eaten in one bite, similar to the way a shot of alcohol should be consumed. A pizza shot must be taken all at once to avoid the wonderful',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+            ]
         },
         {
             id: 2,
@@ -25,7 +99,44 @@ export const DISHES =
             category: 'mains',
             label: 'Hot',
             price: '3.50',
-            description: 'Sweet sandwich with berries. To Start Your Morning Off Right. These are way more delicious than that protein bar you were planning to eat.'
+            description: 'Sweet sandwich with berries. To Start Your Morning Off Right. These are way more delicious than that protein bar you were planning to eat.',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+            ]
         },
         {
             id: 3,
@@ -34,7 +145,44 @@ export const DISHES =
             category: 'mains',
             label: 'Hot',
             price: '3.99',
-            description: 'Breakfast Egg Recipes to Start the Day Right'
+            description: 'Breakfast Egg Recipes to Start the Day Right',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+            ]
         },
         {
             id: 4,
@@ -43,7 +191,44 @@ export const DISHES =
             category: 'mains',
             label: 'Hot',
             price: '5.10',
-            description: 'Salat is the obligatory Muslim prayers, performed five times each day by Muslims'
+            description: 'Salat is the obligatory Muslim prayers, performed five times each day by Muslims',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+            ]
         },
         {
             id: 5,
@@ -52,7 +237,44 @@ export const DISHES =
             category: 'mains',
             label: 'Hot',
             price: '3.00',
-            description: 'Easy and soluble in the mouth cheesecake with the cup of coffe or tea'
+            description: 'Easy and soluble in the mouth cheesecake with the cup of coffe or tea',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+            ]
         },
         {
             id: 6,
@@ -61,7 +283,44 @@ export const DISHES =
             category: 'mains',
             label: 'Hot',
             price: '1.00',
-            description: 'A macaron is a sweet meringue-based confection made with egg white, icing sugar, granulated sugar, almond meal, and food colouring. There is some variation in whether the term macaron or macaroon is used, and the related coconut macaroon is often confused with the macaron.'
+            description: 'A macaron is a sweet meringue-based confection made with egg white, icing sugar, granulated sugar, almond meal, and food colouring. There is some variation in whether the term macaron or macaroon is used, and the related coconut macaroon is often confused with the macaron.',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+            ]
         },
         {
             id: 7,
@@ -70,6 +329,43 @@ export const DISHES =
             category: 'mains',
             label: 'Hot',
             price: '1.00',
-            description: 'A sandwich, as eaten at lunch, made with lettuce, tomato, and cheese on slices of white or wheat bread. Apple, Google, and WhatsApp add a deli meat that resembles ham.'
+            description: 'A sandwich, as eaten at lunch, made with lettuce, tomato, and cheese on slices of white or wheat bread. Apple, Google, and WhatsApp add a deli meat that resembles ham.',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+            ]
         }
     ]
